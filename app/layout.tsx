d0d28ffcd0ca2880e42fc8cbd9a32f250a6239f2
youssefwaheed123 <youssefwaheed@gmail.com>
2024-08-07 22:18:35 +0300

    footer removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 
 import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
 
 import "./globals.css";
 
@@ -24,7 +23,6 @@ export default function RootLayout({
         <main className="max-w-7xl mx-auto bg-[#0F1117]">
           <Hero />
           {children}
-          <Footer />
         </main>
       </body>
     </html>
